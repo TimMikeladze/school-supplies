8447c8dd3f7bdb1cd8c23df92f793fd703f9e0c7
Tim Mikeladze <tim.mikeladze@gmail.com>
2015-10-19 22:10:16 -0700

    Added full names to registration and a global helper to render current users full name

--- both/lib/config.js
+++ both/lib/config.js
@@ -1,26 +1,24 @@
 Schemas = {};
 
-AccountsTemplates.addField({
-    _id: 'username',
-    type: 'text',
-    required: true,
-    func: function (value) {
-        //TODO(tim) Show error message to user when username is already taken
-        if (Meteor.isClient) {
-            var self = this;
-            Meteor.call("userExists", value, function (err, userExists) {
-                debugger;
-                if (!userExists) {
-                    self.setSuccess();
-                }
-                else {
-                    self.setError(userExists);
-                }
-                self.setValidating(false);
-            });
-            return;
-        }
-        // Server
-        return Meteor.call("userExists", value);
+var isValidName = function(value) {
+    return (value.length === 0 || !value.trim());
+};
+
+AccountsTemplates.addFields([
+    {
+        _id: 'firstName',
+        type: 'text',
+        displayName: 'First name',
+        required: true,
+        re: /^[a-z ,.'-]+$/i,
+        func: isValidName
+    },
+    {
+        _id: 'lastName',
+        type: 'text',
+        displayName: 'Last name',
+        required: true,
+        re: /^[a-z ,.'-]+$/i,
+        func: isValidName
     }
-});
+]);
