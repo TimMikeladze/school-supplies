1dcf58e93b39ae66bcfcb4b6761d700eade2e9dc
Tim Mikeladze <tim.mikeladze@gmail.com>
2015-10-21 17:15:28 -0700

    Added first name last name for admin

--- server/superUserSetup.js
+++ server/superUserSetup.js
@@ -1,16 +1,17 @@
 Meteor.startup(function () {
     if (!Meteor.users.findOne()) {
-        console.log("Creating super user account");
+
         var id = Accounts.createUser({
             email: Meteor.settings.admin.email,
             password: Meteor.settings.admin.password,
             username: Meteor.settings.admin.username,
-            firstName: Meteor.settings.admin.firstName,
-            lastName: Meteor.settings.admin.lastName
+            profile: {
+                firstName: Meteor.settings.admin.firstName,
+                lastName: Meteor.settings.admin.lastName
+            }
         });
 
-        Roles.addUsersToRoles(id, ['admin']);
-    } else {
-        console.log("Super user account exists.");
+        Roles.addUsersToRoles(Meteor.users.findOne()._id, ['admin']);
     }
-});
+})
+;
